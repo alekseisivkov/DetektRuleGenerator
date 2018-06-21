46628677152b7407937697a525b602c0216f7688
Semen_Zadorozhnyi <zadorozhnyysemen@gmail.com>
2018-06-21 15:34:59 +0300

    Changed ruleset name.

--- generator/src/main/java/sivkov/alex/com/generator/CustomRuleProvider.kt
+++ generator/src/main/java/sivkov/alex/com/generator/CustomRuleProvider.kt
@@ -3,16 +3,11 @@ package sivkov.alex.com.generator
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
-import sivkov.alex.com.generator.rules.AnotherFunction
 import sivkov.alex.com.generator.rules.ComplicatedIfElseStatement
-import sivkov.alex.com.generator.rules.TooManyFunctions
 
 class CustomRuleProvider : RuleSetProvider {
 
-    override val ruleSetId: String = "CustomRules"
+    override val ruleSetId: String = "custom"
 
-    override fun instance(config: Config) = RuleSet(ruleSetId, listOf(
-            TooManyFunctions(),
-            AnotherFunction(),
-            ComplicatedIfElseStatement()))
+    override fun instance(config: Config) = RuleSet(ruleSetId, listOf(ComplicatedIfElseStatement(config)))
 }
